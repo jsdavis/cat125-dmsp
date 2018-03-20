7ffe17377916487d3b31f32d3dff68f39513c45a
Jacob Davis <jsdavis1010@gmail.com>
2018-03-20 11:56:49 -0700

    Fixing PWA stuff

--- public/sw.js
+++ public/sw.js
@@ -12,6 +12,7 @@ const urlsToCache = [
   // "/assets/icon-96x96.png"
 ];
 
+/*
 self.addEventListener("install", event => {
   // Open a cache for our app
   event.waitUntil(caches.open(CACHE_NAME).then(cache => {
@@ -33,13 +34,14 @@ self.addEventListener("fetch", event => {
       if (!response || response.status !== 200 || response.type !== "basic")
         return response;
 
-      // Cache the response using the request stream
-      // let resToCache = response.clone();
-      // caches.open(CACHE_NAME).then(cache => {
-      //   cache.put(event.request, resToCache);
-      // });
+      Cache the response using the request stream
+      let resToCache = response.clone();
+      caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, resToCache);
+      });
 
       return response;
     });
   }));
 });
+*/
